30edce05c7c1fc2b174d7aeae5838ea13fc39d9b
ariana <ariana.ghasemii@gmail.com>
2024-06-15 10:59:13 +0330

    continue...

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
+const typingBall = document.getElementsByClassName("typingBall");
+const typing  = new Typed('.typed',{
 
+  strings : ["Just Do It!","To-Do List", "Do Your Best!"],
+  typeSpeed: 100,
+  backSpeed:60,
+  loop:true
+// showCursor: true,
+// cursorChar: ''
+})
 
